96c4880b8d5685c4b70fedc9618a93d39fd299c2
fady <gamilfady605@gmail.com>
2022-11-20 13:06:30 +0200

    creating the Entities proejct and define entities models

---
+++ src/CompanyEmployees/Entities/Company.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities;
+public class Company
+{
+    [Column("CompanyId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Company Name Is Required Field")]
+    public string? Name { get; set; } = "";
+
+    [Required(ErrorMessage = "Address Is A Required Field")]
+    public string? Address { get; set; } = "";
+
+    public string? Country { get; set; } = "";
+
+    public IList<Employee>? Employees { get; set; } = new List<Employee>();
+}
---
+++ src/CompanyEmployees/Entities/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities;
+public class Employee
+{
+    [Column("EmployeeId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Employee Name Is A Required Field")]
+    [MaxLength(20, ErrorMessage = "Employee Name Can't Exceed 20 chars")]
+    public string? Name { get; set; } = "";
+
+    [Required(ErrorMessage ="Age Is A Required Field")]
+    public int Age { get; set; }
+
+    [Required(ErrorMessage ="Employee Position Is A Required Field")]
+    [MaxLength (20, ErrorMessage ="Max Length of employee position is 20 chars")]
+    public string? Position { get; set; } = "";
+
+    [ForeignKey(nameof(Company))]
+    public Guid CompanyId { get; set; }
+     public Company Company { get; set; }
+    
+}
+
